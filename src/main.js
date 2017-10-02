6d1fe36d3fb6ed307b595f1605c237def4994537
DESKTOP-O75DFOL\daniel <danielsarcor@gmail.com>
2017-10-02 01:27:23 -0500

    Add routes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+
+import { routes } from './routes'
+Vue.use(VueRouter);
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
 
 // Import Foundation
 import '../node_modules/foundation-sites/assets/foundation.scss'
@@ -9,5 +17,6 @@ import '../node_modules/foundation-sites/dist/js/foundation.js'
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
