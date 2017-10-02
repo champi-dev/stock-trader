604a27624fdc74177f4c802927a1eb6312e783cd
DESKTOP-O75DFOL\daniel <danielsarcor@gmail.com>
2017-10-02 00:47:06 -0500

    Reset

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <template>
   <div id="app">
-      <app-header></app-header>
+
   </div>
 </template>
 
 <script>
-  import Header from './Header.vue'
-
   export default {
     name: 'app',
     mounted() {
       // Set foundation to the document. Necessary for
       // foundation plugins
       $(document).foundation();
-    },
-    components: {
-      appHeader: Header
     }
   }
 </script>
