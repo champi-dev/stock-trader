08767aca498489d93268cd0918ebecaf51960c11
DESKTOP-O75DFOL\daniel <danielsarcor@gmail.com>
2017-10-02 20:51:01 -0500

    Animate routes transition

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <app-header></app-header>
-    <router-view></router-view>
+    <transition name="slide" mode="out-in">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -27,5 +29,35 @@
 <style lang="scss">
   body {
     padding: 20px;
+  }
+
+  .slide-enter-active {
+    animation: slide-in 200ms ease-out forwards;
+  }
+
+  .slide-leave-active {
+    animation: slide-out 200ms ease-out forwards;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateY(-30px);
+      opacity: 0;
+    }
+    to {
+      transform: translateY(0px);
+      opacity: 1;
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateY(0px);
+      opacity: 1;
+    }
+    to {
+      transform: translateY(-30px);
+      opacity: 0;
+    }
   }
 </style>
