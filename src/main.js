5cda296d2d7eb02cb1f30d2b2d0adbb4837654a1
DESKTOP-O75DFOL\daniel <danielsarcor@gmail.com>
2017-10-02 21:05:24 -0500

    Setup vue-resource & firebase

--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import App from './App.vue'
 
 import { routes } from './routes'
+import store from './store/store'
+
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://trader-vue.firebaseio.com/';
+
 const router = new VueRouter({
   mode: 'history',
   routes,
   linkActiveClass: 'active'
 });
-
-import store from './store/store'
 
 Vue.filter('currency', (value) => {
   return '$' + value.toLocaleString();
