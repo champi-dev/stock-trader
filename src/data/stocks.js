a523b82200374a1761ca498423ce294093451605
DESKTOP-O75DFOL\daniel <danielsarcor@gmail.com>
2017-10-04 15:01:20 -0500

    Change stocks algorithm

--- src/data/stocks.js
+++ src/data/stocks.js
@@ -1,6 +1,6 @@
 export default [
-    { id: 1, name: 'BMW', price: 110 },
-    { id: 2, name: 'Google', price: 200 },
-    { id: 3, name: 'Apple', price: 250 },
-    { id: 4, name: 'Twitter', price: 8 }
+    { id: 1, name: 'BMW', price: 170 },
+    { id: 2, name: 'Google', price: 215 },
+    { id: 3, name: 'Apple', price: 232 },
+    { id: 4, name: 'Twitter', price: 182 }
 ];
